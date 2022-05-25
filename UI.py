02b3826e4bf7cc23fa86aa31008737921bd6dc2e
Ali-SZ <samadzadeh110@gmail.com>
2022-05-25 20:51:58 +0430

    main and UI files added main function added to main file MainWindow added to UI file

---
+++ UI.py
@@ -0,0 +1,10 @@
+import imp
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+    def MainWindowInit(self):
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+import UI
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
