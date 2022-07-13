d80680c04ed78ca5f2799cf5d393a73a738d0679
Ali-SZ <samadzadeh110@gmail.com>
2022-07-13 21:58:31 +0430

    signal for Menu Buttons Resource_init.py added to project Icons added to Resource and UI.py

---
+++ Resources_init.py
@@ -0,0 +1,13 @@
+# This is for icons, sql commands and etc.
+
+# ********************* Sizes *********************
+OPEN_MENU_WIDTH = 175
+CLOSED_MENU_WIDTH = 40
+
+# ********************* Icons *********************
+ICONS_LOCATION = "Resources/Icons/"
+MENU_ICON_LOCATION = ICONS_LOCATION + "menu.png"
+MEMBERS_ICON_LOCATION = ICONS_LOCATION + "members.png"
+BOOKS_ICON_LOCATION = ICONS_LOCATION + "books.png"
+BORROWS_ICON_LOCATION = ICONS_LOCATION + "borrow.png"
+SETTING_ICON_LOCATION = ICONS_LOCATION + "setting.png"
--- UI.py
+++ UI.py
@@ -1,11 +1,16 @@
-from PyQt5 import QtWidgets, QtCore, QtGui
+from tkinter import Menubutton
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow,
     QHBoxLayout, QVBoxLayout,
     QListWidget, QListWidgetItem, QPushButton, QStackedWidget, QLabel, QSpacerItem, QLineEdit, QTableWidget,
     QSizePolicy
 )
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import QSize, QPropertyAnimation
+
 from mainwindow_ui import Ui_MainWindow
+from Resources_init import *
 
 import sys
 import pyautogui
@@ -21,6 +26,40 @@ class MainWindow(QMainWindow):
         
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        
+        self.clickedButtons_init()
+        self.icons_init()
+        
+    
+    # ********************* Initializers *********************
+    def clickedButtons_init(self):
+        # - for Menu Buttons
+        self.ui.ToMemebersPageButton.clicked.connect(self.gotoMembersPage)
+        self.ui.ToBooksPageButton.clicked.connect(self.gotoBooksPage)
+        self.ui.ToBorrowPageButton.clicked.connect(self.gotoBorrowsPage)
+        
+    def icons_init(self):
+        # - for Menu Buttons
+        self.ui.MenuButton.setIcon(QIcon(MENU_ICON_LOCATION))
+        self.ui.ToMemebersPageButton.setIcon(QIcon(MEMBERS_ICON_LOCATION))
+        self.ui.ToBooksPageButton.setIcon(QIcon(BOOKS_ICON_LOCATION))
+        self.ui.ToBorrowPageButton.setIcon(QIcon(BORROWS_ICON_LOCATION))
+        self.ui.SettingButton.setIcon(QIcon(SETTING_ICON_LOCATION))
+        
+        # - for Pages
+        self.ui.MembersImage.setPixmap(QPixmap(MEMBERS_ICON_LOCATION))
+        self.ui.BooksImage.setPixmap(QPixmap(BOOKS_ICON_LOCATION))
+    
+    
+    # ********************* Signals *********************
+    # - for Menu Buttons
+    def gotoMembersPage(self):
+        self.ui.Pages.setCurrentIndex(0)
+    def gotoBooksPage(self):
+        self.ui.Pages.setCurrentIndex(1)
+    def gotoBorrowsPage(self):
+        self.ui.Pages.setCurrentIndex(2)
+        
 
 def ShowWindows():
     app = QApplication(sys.argv)
